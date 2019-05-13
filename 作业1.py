86a15dc861b9be63a355b5765a43a36bdfd9ceab
FRIDA Z <2021309158@qq.com>
2019-05-13 13:13:26 +0800

    Hello,teacher an.This is my homework.Thx.

--- 作业1.py
+++
@@ -1,8 +0,0 @@
-result=0
-with open('C:/Users/20213/Documents/CS1.2/Python/git/text-file-process-FridaZ1777/log_files/201811113024.log',encoding='utf8') as t:
-    for line in t:
-        s1=line.split('：')
-        s2=s1[2].split(',')
-        if s2[0]=='201811113024':
-            result+=1
-print(result)
